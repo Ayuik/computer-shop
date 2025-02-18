7ccbeb4661c067b396103d23abd1f0f22d91c7a3
Ayuik <marcosayelen@gmail.com>
2025-02-18 15:32:28 +0100

    changed src. changed printIn

--- src/main/java/dev/ayelen/template_java/App.java
+++ src/main/java/dev/ayelen/template_java/App.java
@@ -1,9 +1,9 @@
-package dev.mabel.template_java;
+package dev.ayelen.template_java;
 public final class App {
     private App() {}
 
     public static void main(String[] args) {
-        System.out.println("My Java Template");
+        System.out.println("Computer shop");
 
     }
 }
---
+++ src/test/java/dev/ayelen/test_java/AppTest.java
@@ -0,0 +1,11 @@
+package dev.ayelen.test_java;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+    @Test
+    void testApp() {
+        
+    }
+}
