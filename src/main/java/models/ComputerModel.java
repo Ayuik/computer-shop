8a6735c9504e11e92b08510720ae470ab1c94095
Ayuik <marcosayelen@gmail.com>
2025-02-21 17:19:16 +0100

    testComputerModel passed

--- src/main/java/models/ComputerModel.java
+++ src/main/java/models/ComputerModel.java
@@ -1,5 +1,35 @@
 package models;
 
 public class ComputerModel {
+    private String brand, memory, processor, operatingSystem;
+    private float price;
+
+    public ComputerModel(String brand, String memory, String processor, String operatingSystem, float price) {
+        this.brand = brand;
+        this.memory = memory;
+        this.processor = processor;
+        this.operatingSystem = operatingSystem;
+        this.price = price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getMemory() {
+        return memory;
+    }
+
+    public String getProcessor() {
+        return processor;
+    }
+
+    public String getOperatingSystem() {
+        return operatingSystem;
+    }
+
+    public float getPrice() {
+        return price;
+    }
 
 }
