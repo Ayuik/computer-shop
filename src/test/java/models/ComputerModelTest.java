aa1e9ff404b5c6a36dcded978c31196c079dd042
Ayuik <marcosayelen@gmail.com>
2025-02-21 17:17:28 +0100

    testComputerModel refactored

--- src/test/java/models/ComputerModelTest.java
+++ src/test/java/models/ComputerModelTest.java
@@ -11,20 +11,12 @@ public class ComputerModelTest {
     @Test
     @DisplayName("Should create a ComputerModel with the following details: brand, processor, memory, operatingSystem and price")
     void testComputerModel() {
-        String brand, memory, processor,operatingSystem;
-        float price;
-        ComputerModel computerModel = new ComputerModel();
-        computerModel.ComputerModel(brand, memory, processor, operatingSystem, price);
-        computerModel.setBrand("Lenovo");
-        computerModel.setMemory("32 GB");
-        computerModel.setProcessor("Intel® Core™ Ultra 7");
-        computerModel.setOperatingSystem("Windows 11 Pro 64");
-        computerModel.setPrice(1999,00f);
+        ComputerModel computerModel = new ComputerModel("Lenovo", "32 GB", "Intel® Core™ Ultra 7", "Windows 11 Pro 64", 1999.00f);
         assertThat(computerModel.getBrand(), is ("Lenovo"));
         assertThat(computerModel.getMemory(), is ("32 GB"));
         assertThat(computerModel.getProcessor(), is("Intel® Core™ Ultra 7"));
         assertThat(computerModel.getOperatingSystem(), is("Windows 11 Pro 64"));
-        assertThat(computerModel.getPrice(), is(1999,00f));        
+        assertThat(computerModel.getPrice(), is(1999.00f));        
     }
     
 }
